f31339dd0dcc41038e27e62306603d9bdf791068
Aleksandr Ufimtsev <fimak@bk.ru>
2021-08-26 18:13:37 +0600

    feat: new commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class='container'>
+    <h1 class='h1'>Hello  times</h1>
+    <div class='btn-group'>
+      <button class='btn btn-success'>+</button>
+      <button class='btn btn-danger'>-</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
